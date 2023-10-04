e44662974fb579a66b893b8dbb595a5db1e8c5f4
samira toubal seghir <samsidt@gmail.com>
2023-10-04 12:58:33 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+function changtext(){
+    const txt=document.getElementById('text');
+    txt.textContent="This is really cool!";
+    return txt
+}
+
+document.addEventListener("DOMContentLoaded", changtext);
+  
